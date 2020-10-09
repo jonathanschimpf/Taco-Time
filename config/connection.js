d0034ee0c57cfb4a815ca2240a5e00c0d991b5a4
Jawnathan <jonathan@jonathanschimpf.com>
2020-10-09 15:34:30 -0400

    fixing syntax

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL) {
-  
-connection = mysql.CreateConnection(process.env.JAWSDB_URL)
+
+connection = mysql.createConnection(process.env.JAWSDB_URL)
 
 } else {
 
